66539b311de3d71a72daa2154ea6146b596e286b
Alvaro Japa Salazar <alvaruwiiu@hotmail.com>
2022-10-29 13:25:44 -0500

    first commit

---
+++ challenge/frontend/src/services/bookings.ts
@@ -0,0 +1,30 @@
+// use axios to fetch data from the backend API called CHALLENGE_API
+import { CHALLENGE_API } from "../queries/endpoints";
+import axios from "axios";
+
+import { BookingResponse, } from "./types";
+import { AxiosResponse } from "axios";
+
+export const getBookings = async () => {
+  const response = await axios.get(`${CHALLENGE_API}/bookings`);
+  return response.data;
+}
+
+export const createBooking = async (
+  data: BookingResponse,
+): Promise<AxiosResponse<BookingResponse>> => {
+  const response = await axios.post(`${CHALLENGE_API}/bookings/`, data);
+  return response.data;
+}
+
+export const deleteBooking = async (id: number) =>{   
+  const response = await axios.delete(`${CHALLENGE_API}/bookings/${id}/`);
+  return response;
+}
+
+
+export const BookingsService = {
+  getBookings,
+  createBooking,
+  deleteBooking
+};
